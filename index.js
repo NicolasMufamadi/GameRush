f7bb2910292188476ac34ba18e54ba892a9961f5
NicolasMufamadi <mufamadianwani@gmail.com>
2021-10-12 19:56:46 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//routes
+const UserRoutes = require('./Routes/UserRoutes/userRoutes') 
+const ProductRoutes = require('./Routes/ProductRoutes/productRoutes')
+
+app.use('/users',UserRoutes);
+app.use('/products',ProductRoutes)
+
+app.listen(process.env.PORT,()=>console.log(`Server up and running on http:localhost:${process.env.PORT}`))
