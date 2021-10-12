af61afdc7ad3486c0a425255222fd5c5f8459671
NicolasMufamadi <mufamadianwani@gmail.com>
2021-10-12 19:47:26 +0200

    added config

---
+++ config/UserType.js
@@ -0,0 +1,35 @@
+module.exports = ()=>{
+
+  const UserType =  [
+
+        {
+          
+            Type: 'Admin',
+            id:  1
+
+        },
+
+        {
+
+            Type: 'Order Manager',
+            id: 2
+
+        },
+
+        {
+
+            Type: 'Product Manager',
+            id: 3 
+
+        },
+
+        {
+            Type: 'Customer',
+            id: 4
+        }
+
+    ]
+
+    return UserType
+
+}
