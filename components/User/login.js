78076404e78c4692c7d38d47e053c098259c5e19
NicolasMufamadi <mufamadianwani@gmail.com>
2021-10-13 17:09:37 +0200

    git add frontend/

--- components/User/login.js
+++ components/User/login.js
@@ -1,16 +1,34 @@
 const db = require('../../database/database')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const User = require('../../models/User');
 
 module.exports = (req,res)=>{
 
-    db.User.findAll({
-  //check if the email entered is in the database 
-        where: {
-            Email: req.body.Email,
-        }
-        
-    }).then(data=>{
-        console.log(data);
+     //check if the email entered is in the database 
+    db.User.findOne({where: {Email: req.body.Email}})
+    .then(data=>{
+        if(data){
+            //compare input password with the password in the database 
+          bcrypt.compare(req.body.Password,data.dataValues.Password,(err,response)=>{
+             if(err){
+                 res.status(400).send(err)
+             }
+             if(response){
+                const token = jwt.sign({...data},process.env.SECRET,{
+                    expiresIn: "1h"
+                })
+
+                res.status(200).send({message:'Logged In', token: token, data: data.dataValues})
+             }else {
+                 res.status(400).send({error:{message: 'Incorrect password'}})
+             }
+          })
+       } else res.status(400).send({error:{message:'Email does not exist'}})
+
+    }).catch(err=>{
+        console.log(err);
     })
 
 }
