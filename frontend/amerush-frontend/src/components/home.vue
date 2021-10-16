46569058c1240f9dbb8097b5bdd35d7ea123a963
NicolasMufamadi <mufamadianwani@gmail.com>
2021-10-16 12:38:29 +0200

    added home

---
+++ frontend/amerush-frontend/src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <Header/>
+</template>
+
+
+<script>
+import Header from './header.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+    color: #fff;
+    font-size: 25px;
+}
+
+body{
+      background: #1F2833;
+}
+</style>
