7e09a23d03c3ee7abce4bbc5a25dfa3ed4f2efc9
NicolasMufamadi <mufamadianwani@gmail.com>
2021-12-03 01:40:08 +0200

    updated database

--- database/database.js
+++ database/database.js
@@ -1,11 +1,12 @@
 const {Sequelize} = require('sequelize')
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 //connect to database 
 
 const sequelize = new Sequelize( process.env.DATABASE , process.env.USER , process.env.PASSWORD ,{
 
     host: process.env.HOST, 
-    dialect: process.env.DIALECT 
+    dialect: process.env.DIALECT
 
 })
 
@@ -16,6 +17,6 @@ db.sequelize = sequelize;
 
 db.User = require('../models/User')(sequelize,Sequelize);
 db.Product = require('../models/product')(sequelize,Sequelize)
-//db.sequelize.sync({force:true});
+db.sequelize.sync({force:true});
 
 module.exports = db;
