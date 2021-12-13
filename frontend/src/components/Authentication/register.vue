ae254288218188d905fd392405166b56142b7863
NicolasMufamadi <mufamadianwani@gmail.com>
2021-12-13 04:20:24 +0200

    Added logiregister auth

--- frontend/src/components/Authentication/register.vue
+++ frontend/src/components/Authentication/register.vue
@@ -13,19 +13,25 @@
             <v-text-field
                v-model="FirstName" 
                label="First Name"
+                  :error="FirstName_err ? true : false"
+                 :error-messages="FirstName_err"
                color="#1F2833"
-               required
                solo
-              hide-details>
+               hide-details = "auto"
+              >
             </v-text-field>
+
           </v-col>
           <v-col cols="12" sm="12" ms="1">
             <v-text-field    
                  v-model="LastName"
                  label="Last Name" 
+                 :error="LastName_err ? true : false"
+                 :error-messages="LastName_err"
                  color="#1F2833" 
                  solo
-                 hide-details>
+                 hide-details="auto"
+                 >
             </v-text-field>
           </v-col>
           <v-col cols="12" sm="6" ms="1">
@@ -33,8 +39,10 @@
               v-model="Email"
               label="Email"
               type="email"
+              :error="Email_err ? true : false"
+              :error-messages ="Email_err"
               solo
-              hide-details
+              hide-details = "auto"
             >
             </v-text-field>
           </v-col>
@@ -43,9 +51,10 @@
               v-model="ConfirmEmail"
               label="Confirm-Email"
               type="email"
-              required
+              :error="ConfirmEmail_err ? true : false"
+              :error-messages="ConfirmEmail_err"
               solo
-              hide-details
+              hide-details = "auto"
             >
             </v-text-field>
           </v-col>
@@ -55,8 +64,10 @@
                v-model="Password"
                label="Password"
                type="password"
+               :error="Password_err ? true : false"
+               :error-messages="Password_err"
                solo
-               hide-details   
+               hide-details  = "auto"
             >
             </v-text-field>
           </v-col>
@@ -64,10 +75,11 @@
             <v-text-field
                 v-model="ConfirmPassword"
                 label="Confirm-Password"
+                :error="ConfirmPassword_err ? true : false"
+                :error-messages="ConfirmPassword_err"
                 type="password"
-                required
                 solo
-                hide-details
+                hide-details = "auto"
             >
             </v-text-field>
           </v-col>
@@ -76,9 +88,10 @@
             <v-text-field 
                v-model="Phone"
                label="Phone Number" 
-               required 
+               :error="Phone_err ? true : false"
+               :error-messages="Phone_err"
                solo 
-               hide-details
+               hide-details = "auto"
             >
             </v-text-field>
           </v-col>
@@ -87,28 +100,34 @@
     </v-card-text>
     <v-card-actions>
       <div>
-     <v-btn @click="submitForm" color="#66FCF1" lighht  depressed class="mx-2">
+     <v-btn @click="submitForm" 
+            color="#66FCF1" 
+            lighht 
+            depressed
+            class="mx-2"
+             >
          Register
      </v-btn>
       </div>
       <div>
+        
         <v-btn  v-if="done" @click="reset" color="error" depressed light class="mx-2">
            Close
         </v-btn>
       </div>
     </v-card-actions>
   </v-card>
+
 </template>
 
 <script>
 
 import axios from 'axios'
-
+import Validator from 'validator'
 export default {
   name: "register",
    props: ["done"],
    data: ()=>({
-      
       FirstName: '',
       LastName: '',
       Email: '',
@@ -116,37 +135,136 @@ export default {
       Password: '',
       ConfirmPassword: '',
       Phone: '',
-      UserType: 4
+      UserType: "Customer",
+
+      FirstName_err: null,
+      LastName_err: null,
+      Email_err: null,
+      ConfirmEmail_err: null,
+      ConfirmPassword_err: null,
+      Password_err: null,
+      Phone_err: null,
     
 
    }),
    methods: {
 
-    async submitForm(e){
+   submitForm(e){
 
-       e.preventDefault();
 
-       let response = await axios.post('http://localhost:8025/users/register',{
-            FirstName: this.FirstName,
-            LastName: this.LastName,
-            Email: this.Email,
-            Password: this.Password,
-            Phone: this.Phone,
-            UserType: this.UserType
-         })
-           
-           if(response){
-             this.done()
+
+       e.preventDefault()
+        
+        
+     if(!this.validate()) return false
+   
+
+            axios.post('http://localhost:8025/users/register',{
+              FirstName: this.FirstName,
+              LastName: this.LastName,
+              Email: this.Email,
+              Password: this.Password,
+              Phone: this.Phone,
+              UserType: this.UserType
+           }).then(response=>{
+             
+             console.log(response)
              this.reset()
-           }else{
-             console.log('error')
-           }
- 
+             this.done()
+
+             })
+           .catch(error=>{
+             
+            if(error){
+              this.Email_err = "Email already exists"
+            }
+        
+             
+             })
+
+         },
+
+    
+     validate(){
+      
+        let valid = true
+       
+        if(!this.Email && !this.FirstName && !this.LastName  && !this.Password && !this.Phone ){
+                      this.Email_err = "Required"
+                      this.FirstName_err = "Required"
+                      this.LastName_err = "Required"
+                      this.Password_err = "Required"
+                      this.Phone_err = "Required"
+                      valid = false
+            } 
+
+         if(this.FirstName.length < 4){
+             this.FirstName_err = "FirstName must have at least 4 characters"
+             valid = false
+         }  else{
+           this.FirstName_err = null
+         }
+
+        if(this.LastName.length < 4 ){
+          this.LastName_err = "LastName must have at least 4 characters"
+           valid = false
+        }else{
+          this.LastName_err = null
+        } 
+
+        if(!Validator.isEmail(this.Email)){
+           this.Email_err = "Invalid Email"
+            valid = false
+        }else{
+          this.Email_err = null
+        }
+
+        if( Validator.isEmail(this.Email) && this.Email !== this.ConfirmEmail){
+           this.ConfirmEmail_err = "Email does not match"
+            valid = false 
+        }else{
+          this.ConfirmEmail_err = null
+        }           
+
+        let PasswordStrength = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})")
+
+        if(!PasswordStrength.test(this.Password)){
+          
+          this.Password_err = "At least 8 characters, 1 uppercase,1 lowercase, 1 digit & 1 special character"
+          valid = false
+
+        }else{
+          this.Password_err = null
+        }
+
+
+        if( PasswordStrength.test(this.Password) && this.Password !== this.ConfirmPassword){
+          this.ConfirmPassword_err = "Password does not match"
+          valid = false 
+        }else{
+          this.ConfirmPassword_err = null
+        }
+           
+         if(!this.ValidateNumber(this.Phone) || this.Phone.length > 12){
+             this.Phone_err = "Invalid Number"
+             valid = false
+         }else{
+           this.Phone_err = null
+         }
+          
+          return valid       
      },
+    
+     ValidateNumber(number){
+      let phonReg = new RegExp("[0|+27]{1}[0-9]{9,}")
+
+         return phonReg.test(number);
+     },
+     
 
      reset(){
 
-        
+             
         this.FirstName = '';
         this.LastName = '';
         this.Email = '';
@@ -154,8 +272,18 @@ export default {
         this.Password = '';
         this.ConfirmPassword = '';
         this.Phone = '';
+        
+        this.FirstName_err = null
+        this.LastName_err = null
+        this.Email_err = null
+        this.ConfirmEmail_err = null
+        this.Password_err = null
+        this.ConfirmPassword_err = null
+        this.Phone_err = null
+        
 
        if(this.done) this.done();
+
      }
    }
 };
