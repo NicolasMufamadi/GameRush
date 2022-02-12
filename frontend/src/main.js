54068c98ba3966fbc27bcd526aa24395c28143b7
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-02-12 23:34:53 +0200

    added store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
 
 Vue.config.productionTip = false
 Vue.use(Vuetify,{
@@ -13,6 +15,7 @@ const vuetify = new Vuetify({});
 
 new Vue({
   vuetify,
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
