93523569aade773422b551253caff6d0be8f4392
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-03 19:40:24 +0200

    added updating names form

--- frontend/src/components/UserAccount/profileforms/NameForm.vue
+++ frontend/src/components/UserAccount/profileforms/NameForm.vue
@@ -10,19 +10,33 @@
       <form>
         <div>
           <div>
-            <v-text-field label="First Name" filled color="#1F2833">
+            <v-text-field 
+                label="First Name" 
+                filled color="#1F2833"
+                v-model="FirstName"
+                :error= "firstname_err ? true : false"
+                :error-messages="firstname_err"    
+               
+             >
             </v-text-field>
           </div>
           <div>
-            <v-text-field label="Last Name" filled color="#1F2833">
+            <v-text-field
+               label="Last Name"
+               filled color="#1F2833"
+               v-model="LastName"
+               :error= "lastname_err ? true : false"
+               :error-messages="lastname_err"
+
+            >
             </v-text-field>
           </div>
         </div>
  
            <div class="d-flex justify-end">
             
-               <v-btn class="mx-5" color="error" large outlined >Cancel</v-btn>
-               <v-btn color="#1F2833" large outlined>Update</v-btn>
+               <v-btn @click="cancel" class="mx-5" color="error" large outlined>Cancel</v-btn>
+               <v-btn @click="update" color="#1F2833" large outlined>Update</v-btn>
 
            </div>
 
@@ -33,10 +47,103 @@
 </template>
 
 <script>
+
 import UserCard from "../UserCard.vue";
+import {mapGetters} from "vuex"
+import axios from 'axios'
+
 export default {
   name: "NameForm",
-  components: { UserCard },
+  
+  components: { 
+    UserCard
+     },
+
+     data:()=>({
+
+         FirstName: '',
+         LastName: '',
+         Email: '',
+         Password: '',
+         Phone: '',
+
+         firstname_err: '',
+         lastname_err: '',
+
+         Id: ''
+
+     }),
+  
+  computed:{
+     ...mapGetters(['user']),
+  },
+
+
+   
+        created(){
+       
+         this.Id = this.user.data.UserId
+         this.FirstName = this.user.data.FirstName
+         this.LastName = this.user.data.LastName
+
+ 
+     },
+
+
+  methods:{
+
+    async  update(e){
+       
+       e.preventDefault()
+       if(!this.validate()) return false
+
+      await   axios.put(`http://localhost:8000/users/updateuser/name/${this.Id}`,
+        {
+          FirstName: this.FirstName,
+          LastName: this.LastName,
+
+        }).then(data=>{
+             console.log(data)
+            this.$store.dispatch('getauth',data)
+             this.$router.push('/myaccount/personal-details/')
+        }).catch(err=>{
+          console.log(err)
+        })
+        
+     },
+
+     validate(){
+ 
+       let valid = true
+
+       if(this.FirstName.length < 3){
+            this.firstname_err = 'A minimum of 3 characters is required'
+            valid = false
+       }else if(this.LastName.length < 3){
+         this.lastname_err = 'A minimum of 3 characters is required'
+         valid = false
+       }else{
+
+         this.firstname_err = ''
+         this.lastname_err = ''
+         valid = true
+ 
+        }
+            return valid 
+     },
+
+
+
+     cancel(){
+
+         this.$router.push('/myaccount/personal-details/')
+     },
+
+     //...mapActions(['getauth'])
+
+  }
+  
+
 };
 </script>
 
