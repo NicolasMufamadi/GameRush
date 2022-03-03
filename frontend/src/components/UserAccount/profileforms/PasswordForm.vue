95d003f8aeb8018d64734256a144e4f442d1adb4
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-03 19:37:47 +0200

    added updating password form

--- frontend/src/components/UserAccount/profileforms/PasswordForm.vue
+++ frontend/src/components/UserAccount/profileforms/PasswordForm.vue
@@ -12,29 +12,44 @@
 
            <div>
                <form>
-                 <div>
-
-                    
-                   <v-text-field label="Current Password" filled color="#1F2833">
-
-                   </v-text-field>
-                </div>
-
                 <div>
-                    <v-text-field label="New Password" filled color="#1F2833">
+                    <v-text-field 
+                    
+                         label="New Password" 
+                         filled color="#1F2833"
+                         v-model="NewPassword"
+                         :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                         :type="show2 ? 'text' : 'password'"
+                         :error="new_password_err ? true : false"
+                         :error-messages="new_password_err"
+                         hint="At least 8 characters, 1 uppercase,1 lowercase, 1 digit & 1 special character"
+                         @click:append="show2 = !show2"
+                    
+                    >
 
                     </v-text-field>
                 </div>
 
                 <div>
-                    <v-text-field label="Confirm New Password" filled color="#1F2833">
+                    <v-text-field  
+                    
+                        label="Confirm New Password" 
+                        filled color="#1F2833"
+                        v-model="ConfirmNew"
+                        :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="show3 ? 'text' : 'password'"
+                        :error="confirm_password_err ? true : false"
+                        :error-messages="confirm_password_err"
+                        @click:append="show3 = !show3"
+                        
+                    >
 
                     </v-text-field>
                 </div>
 
                 <div  class="d-flex justify-end">
-                    <v-btn class="mx-5" color="error" large outlined>Cancel</v-btn>
-                    <v-btn color="#1F2833" large outlined>Update</v-btn>
+                    <v-btn @click="cancel" class="mx-5" color="error" large outlined>Cancel</v-btn>
+                    <v-btn @click="updatePassword" color="#1F2833" large outlined>Update</v-btn>
                 </div>
 
                </form>
@@ -46,12 +61,117 @@
 </template>
 
 <script>
+
 import UserCard from '../UserCard.vue'
+import axios from 'axios'
+import {mapGetters} from 'vuex'
+import bcrypt from 'bcryptjs'
+
 export default {
    name: "PasswordForm",
    components:{
       UserCard
-   }    
+   },
+   
+   data:()=>({
+
+
+       Password: '',
+       NewPassword: '',
+       ConfirmNew: '',
+
+       show2: false,
+       show3: false,
+
+       new_password_err: '',
+       confirm_password_err: ''
+
+
+   }),
+
+   computed:{
+        ...mapGetters(['user'],['UserId'])
+   },
+
+   created(){
+        
+         this.Id = this.user.data.UserId
+         this.Password = this.user.data.Password
+
+   },
+
+   methods:
+   {
+          
+          async updatePassword(e){
+                
+                e.preventDefault()
+
+            if(!this.validate()) return false
+
+               await axios.put(`http://localhost:8000/users/updateuser/password/${this.Id}`,
+               {
+
+                     Password: this.NewPassword,
+
+               }).then(data=>{
+                   
+                   this.$store.dispatch('getauth',data)
+                   this.$router.push('/myaccount/personal-details/')
+
+               }).catch(err=>{
+                   console.log(err)
+               })
+
+          },
+
+
+          validate(){
+
+              let valid = true
+
+              bcrypt.compare(this.CurrentPassword,this.Password,(err,response)=>{
+                  if(err){
+                      console.log(this.CurrentPassword +''+ this.Password)
+                      this.current_passwor_err = 'Current Password not matching'
+                      valid = false
+                  }
+
+                  if(response){
+                     this.current_passwor_err = null   
+                  }
+                  
+              })
+
+             let PasswordStrength = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})")
+              if(!PasswordStrength.test(this.NewPassword)){
+          
+             this.new_password_err = "At least 8 characters, 1 uppercase,1 lowercase, 1 digit & 1 special character"
+              valid = false
+          }else{
+            this.new_password_err = null
+          }
+            if( PasswordStrength.test(this.NewPassword) && this.NewPassword !== this.ConfirmNew){
+               this.confirm_password_err = "Password does not match"
+               valid = false 
+        }else{
+               this.confirm_password_err = null
+           }
+            
+            return valid
+
+    },
+
+          cancel(){
+
+             this.$router.push('/myaccount/personal-details/')
+
+          }
+
+   }
+
+
+   
 }
 </script>
 
