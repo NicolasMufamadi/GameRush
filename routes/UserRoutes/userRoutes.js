cf89fde7ba9a83c3830be113ad5678c91d941c8a
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-03 19:45:09 +0200

    updated userRoutes.js

--- routes/UserRoutes/userRoutes.js
+++ routes/UserRoutes/userRoutes.js
@@ -10,13 +10,20 @@ const getUser  = require('../../components/User/getUser')
 const deleteUser = require('../../components/User/RemoveUser')
 const login   = require('../../components/User/login')
 const logout = require('../../components/User/logout')
+const updateName = require('../../components/User/updateName')
+const updateEmail = require('../../components/User/updateEmail');
+const updatePhone = require('../../components/User/updatePhone');
+const updatePassword = require('../../components/User/updatePassword')
 
 router.post("/register",registerUser)
 router.post("/login",login)
 router.post('/logout',logout)
 router.get('/getusers',getUsers)
-router.get('/getuser/:UserId',auth,getUser)
-router.put('/updateuser/:UserId',auth,updateUser)
+router.get('/getuser/:UserId',getUser)
+router.put('/updateuser/name/:UserId',updateName)
+router.put('/updateuser/email/:UserId',updateEmail)
+router.put('/updateuser/phone/:UserId',updatePhone)
+router.put('/updateuser/password/:UserId',updatePassword)
 router.delete('/deleteuser/:UserId',deleteUser)
 
 
