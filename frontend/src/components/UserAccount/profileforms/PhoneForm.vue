e398a7710010443a416921fb2a44ee1c47633d79
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-03 19:36:44 +0200

    added updating phone form

--- frontend/src/components/UserAccount/profileforms/PhoneForm.vue
+++ frontend/src/components/UserAccount/profileforms/PhoneForm.vue
@@ -13,19 +13,19 @@
             <div>
                 <form >
                     <div>
-                         <v-text-field label="Current Numbers" filled color="#1F2833">
+                         <v-text-field 
+                                 v-model="Phone" 
+                                 label="Change Numbers" 
+                                 filled color="#1F2833"
+                                 :error="Phone_err ? true : false"
+                                 :error-messages="Phone_err"       
+                         >
 
                          </v-text-field>
                     </div>
-                    <div>
-                        <v-text-field label="New Numbers" filled color="#1F2833">
-
-                        </v-text-field>
-
-                    </div>
                  <div  class="d-flex justify-end">
-                    <v-btn class="mx-5" color="error" large outlined>Cancel</v-btn>
-                    <v-btn color="#1F2833" large outlined>Update</v-btn>
+                    <v-btn @click="cancel" class="mx-5" color="error" large outlined>Cancel</v-btn>
+                    <v-btn @click="updateNumbers" color="#1F2833" large outlined>Update</v-btn>
                 </div>
                 </form>
             </div>
@@ -37,12 +37,82 @@
 <script>
 
 import UserCard from '../UserCard.vue'
+import {mapGetters} from 'vuex'
+import axios from 'axios'
 
 export default {
     name: "PhoneForm",
     components: {
         UserCard
-    }    
+    },
+    
+    data:()=>({
+         
+        Id: '',
+        Phone: '',
+
+        Phone_err: ''
+
+    }),
+
+    computed:
+    {
+        ...mapGetters(['user'])         
+    },
+
+     created(){
+
+         this.Id = this.user.data.UserId
+         this.Phone = this.user.data.Phone
+
+       },
+
+    methods:
+    {
+          async updateNumbers(e){
+               
+               e.preventDefault()
+
+              if(!this.validate()) return false
+
+              await axios.put(`http://localhost:8000/users/updateuser/phone/${this.Id}`,
+              {
+
+                 Phone: this.Phone
+
+              }).then(data=>{
+                      this.$store.dispatch('getauth',data)
+                      this.$router.push('/myaccount/personal-details/')
+       
+              }).catch(err=>{
+                  console.log(err)
+              })
+          },
+
+      validate(){
+ 
+           let valid = true
+        if(!this.ValidateNumber(this.Phone) || this.Phone.length > 12){
+             this.Phone_err = "Enter valid RSA Number"
+             valid = false
+         }else{
+           this.Phone_err = null
+         }
+          
+          return valid       
+     },
+
+
+      ValidateNumber(number){
+         let phonReg = new RegExp("[0|+27]{1}[0-9]{9,}")
+         return phonReg.test(number);
+     },
+
+      cancel(){
+          this.$router.push('/myaccount/personal-details/')
+      }
+
+    }
 }
 </script>
 
