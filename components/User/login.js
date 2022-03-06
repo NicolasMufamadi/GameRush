16c77b303902f55c9b3c8ef4c48cebdef60c87c5
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-06 13:41:23 +0200

    modified token expiration time

--- components/User/login.js
+++ components/User/login.js
@@ -13,22 +13,24 @@ module.exports = (req,res)=>{
         if(data){
             //compare input password with the password in the database 
           bcrypt.compare(req.body.Password,data.dataValues.Password,(err,response)=>{
-             if(err){
-                 res.status(400).send(err)
-             }
+
              if(response){
-                const token = jwt.sign({...data},process.env.SECRET,{
-                    expiresIn: "15s"
+                const token = jwt.sign({UserId:data.dataValues.UserId,Role:data.dataValues.UserType},process.env.SECRET,{
+                    expiresIn: "30m"
                 })
                 res.status(200).send({message:'Logged In', token: token, data: data.dataValues})
             }else {
-                 res.status(403).send({error:{message: 'Incorrect password'}})
+                 console.log(err)
+                 res.status(400).send({error:{message: 'Incorrect Password'}})
              }
           })
-       } else res.status(400).send({error:{message:'Email does not exist'}})
+        }else{
+
+            res.status(404).send({error:{message:'Email does not exist'}})
+        }
 
     }).catch(err=>{
-        console.log(err);
+        console.log(err)
     })
 
 
