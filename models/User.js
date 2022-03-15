540aefde1ba3d547627da54c8ad108ea0f950464
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-03-15 15:16:49 +0200

    Added IsBanned data column

--- models/User.js
+++ models/User.js
@@ -44,6 +44,11 @@ module.exports = (sequelize,Sequelize) =>{
             type: Sequelize.STRING,
             defaultValue: "Customer",
           
+        },
+
+        IsBanned:{
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
         }
 
     })
