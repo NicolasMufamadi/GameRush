c53c54d84f77afa4e9aaddcd4b1840707a17c37d
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-04-06 16:41:07 +0200

    added productManagement in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
-//import userState from './modules/userState'
+import manageProduct from './modules/manageProduct'
 import  authUser from './modules/authUser'
 
 
@@ -17,8 +17,8 @@ const store = new Vuex.Store({
 
     modules:{
 
-       
-        authUser
+        authUser,
+        manageProduct
 
     },
     plugins: [createPersistedState()]
