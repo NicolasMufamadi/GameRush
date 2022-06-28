faeff9eb2478263d218aacd397f4b90c250ed70d
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-06-28 16:47:20 +0200

    Added product description component

---
+++ frontend/src/components/Product/productDescription.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+         
+         <v-card>
+             <v-card-title class="cyan--text">
+                 Description
+             </v-card-title>
+
+             <v-card-text class="black--text">
+                 {{product.ProductDesc}}
+             </v-card-text>
+         </v-card>
+
+   </div>    
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+//import axios from 'axios'
+
+export default {
+    name: 'productDescription',
+    
+    computed:{
+         ...mapGetters(['product','user'])
+    },
+
+
+    created(){
+        
+        this.$store.dispatch('authenticateUser')
+    
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
