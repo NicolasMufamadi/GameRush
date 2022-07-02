837d83a5a658c014fd966bf8d22fa8701c7a4368
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-07-02 23:20:49 +0200

    added order shippingaddress card

---
+++ frontend/src/components/Order/orderShippingAddress.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card class="cyan--text py-5" color="#1F2833" width="500px">
+
+          <h1 class="mb-2 cyan--text text-center">Shipping Address</h1>
+          <h3 class="ml-2">Recipient Name: {{recipientName}}</h3>
+          <h3 class="ml-2">Phone Number: {{phone}}</h3>
+          <h3 class="ml-2">Street Address: {{streetAddress}}</h3>
+        
+          <h3 class="ml-2">City:  {{city}}</h3>
+          <h3 class="ml-2">Suburb: {{suburb}}, {{code}}</h3>
+
+    </v-card>
+</template>
+
+<script>
+export default {
+
+    name: 'orderShippingAddress',
+    props: ['recipientName','phone','streetAddress','Near Building','city','suburb','code']
+
+}
+</script>
+
+<style>
+
+</style>
