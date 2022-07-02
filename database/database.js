91e521c969a89122a7ad6ff990e8b066204d05e3
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-07-02 23:26:20 +0200

    added review model schema to the database

--- database/database.js
+++ database/database.js
@@ -23,6 +23,7 @@ db.Cuopon = require('../models/coupon')(sequelize,Sequelize)
 db.Cart = require('../models/cart')(sequelize,Sequelize)
 db.AddressBook = require('../models/addressBook')(sequelize,Sequelize)
 db.Order = require('../models/order')(sequelize,Sequelize)
+db.Review = require('../models/review')(sequelize,Sequelize)
 
 //db.sequelize.sync({force:true})
 
