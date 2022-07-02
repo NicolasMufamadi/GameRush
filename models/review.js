584fa6c21f6b4531d392cc0e3d6a80f3989b03eb
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-07-02 23:19:12 +0200

    added review model

---
+++ models/review.js
@@ -0,0 +1,45 @@
+
+module.exports = (sequelize,Sequelize) =>{
+
+    var review = sequelize.define('Review',{
+        
+        ReviewId: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true   
+        },
+
+        UserId: {
+            type: Sequelize.INTEGER
+        },
+
+        ProductId: {
+            type: Sequelize.INTEGER
+        },
+
+        ProductName: {
+           type: Sequelize.STRING
+        },
+
+        Name: {
+            type: Sequelize.STRING
+        },
+
+        Comment:{
+           type: Sequelize.STRING      
+        },
+
+        Rating: {
+           type: Sequelize.INTEGER 
+        },
+
+        Status: {
+            type: Sequelize.STRING
+        }
+        
+
+    })
+
+  return review
+
+}
