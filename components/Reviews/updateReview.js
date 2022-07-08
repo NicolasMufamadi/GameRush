ac508bd2123518c3f0fa2030160895283d4f77ec
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-07-08 02:14:25 +0200

    added updateReview.js

---
+++ components/Reviews/updateReview.js
@@ -0,0 +1,14 @@
+const db = require('../../database/database')
+
+
+module.exports = (req,res) => {
+
+    db.Review.findOne({where: {ReviewId: req.query.ReviewId}})
+    .then(data=>{
+        data.update({
+            Status: req.body.Status
+        }).then(response=>res.status(200).send(response))
+        .catch(err=>res.status(400).send(err))
+    }).catch(err=>res.status(404).send(err))
+
+}
