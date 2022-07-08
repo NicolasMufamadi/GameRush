d0a842f56cf18f8ba0afec0ee463c842d59080dd
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2022-07-08 02:28:57 +0200

    added date of review

--- components/Reviews/addReview.js
+++ components/Reviews/addReview.js
@@ -2,11 +2,16 @@ const db = require('../../database/database')
 
 module.exports = (req,res) => {
    
+    let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    let date = new Date()
+    let reviewedOn = date.getDate() +' '+ months[date.getMonth()] +' '+ date.getFullYear() 
+
     db.Review.create({
         
         UserId: req.body.UserId,
         ProductId: req.body.ProductId,
         Name: req.body.Name,
+        ReviewedOn: reviewedOn,
         Comment: req.body.Comment,
         Rating: req.body.Rating,
         ProductName: req.body.ProductName,
